26fc5b71a0c7ccdb1177e23053d5d062319368d4
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-08-15 19:59:31 +0100

    so far

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,26 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 
-let client = require('contentful').createClient({
+let client = require("contentful").createClient({
 space: process.env.NEXT_CONTENTFUL_SPACE_ID,
 accessToken: process.env.NEXT_CONTENTFUL_ACCESS_TOKEN,
 });
 
-export default function Home() {
+export async function getStaticProps(){
+  let data = await client.getEntries(
+    {
+      content_type: 'article'
+    })
+    
+  return{
+    props:{
+      articles: data.items
+    }
+  }
+}
+
+export default function Home({articles}) {
+  console.log(articles)
   return (
     <div className={styles.container}>
       <Head>
